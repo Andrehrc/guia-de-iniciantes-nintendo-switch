e954451601e3ef4beddd18d12f858fb4af39897d
André Henrique <andre.henrique0207@gmail.com>
2022-07-25 17:35:46 -0300

    Primeiro Commit, base do projeto pronta, necessário preencher informações em Franquias Destaque

---
+++ assets/javascript/games.js
@@ -0,0 +1,56 @@
+let marioJson = [
+    {name: 'Super Mario Bros', pic: '../images/mario1.png', desc: '', video: 'https://www.youtube.com/embed/1OpjeZMCsEU', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario Bros 2', pic: '../images/mario2.png', desc: '', video: 'https://www.youtube.com/embed/ZU6_I-6lPj8', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario Bros 3', pic: '../images/mario3.png', desc: '', video: 'https://www.youtube.com/embed/1OpjeZMCsEU', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario World', pic: '../images/mario4.png', desc: '', video: 'https://www.youtube.com/embed/esNJUSMsDG4', shop: 'https://www.nintendo.com/store/products/super-nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: "Super Mario World 2 - Yoshi's Island", pic: '../images/mario4-2.png', desc: '', video: 'https://www.youtube.com/embed/esNJUSMsDG4', shop: 'https://www.nintendo.com/store/products/super-nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario All Stars', pic: '../images/mario5.png', desc: '', video: 'https://www.youtube.com/embed/esNJUSMsDG4', shop: 'https://www.nintendo.com/store/products/super-nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario Odyssey', pic: '../images/mario6.png', desc: '', video: 'https://www.youtube.com/embed/wGQHQc_3ycE', shop: 'https://www.nintendo.com/store/products/super-mario-odyssey-switch/'},
+    {name: 'Super Mario 3D All Stars', pic: '../images/mario7.png', desc: '', video: 'https://www.youtube.com/embed/CPJcaGWoO2c', shop: 'https://www.nintendo.com/whatsnew/super-mario-3d-all-stars-leaves-nintendo-eshop-on-march-31st/'},
+    {name: 'New Super Mario Bros U. Deluxe', pic: '../images/mario8.png', desc: '', video: 'https://www.youtube.com/embed/0LbHBMtHUw0', shop: 'https://www.nintendo.com/store/products/new-super-mario-bros-u-deluxe-switch/'},
+    {name: "Super Mario 3D World + Bowser's Fury", pic: '../images/mario9.png', desc: '', video: 'https://www.youtube.com/embed/OPAQA_P_RQY', shop: 'https://www.nintendo.com/store/products/super-mario-3d-world-plus-bowsers-fury-switch/'}
+]
+let marioJsonSpinoff = [
+    {name: 'Wrecking Crew', pic: '../images/mariospin1.png', desc: '', video: '', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario Bros - Lost Levels', pic: '../images/mariospin2.png', desc: '', video: 'https://www.youtube.com/embed/UJDYwXNYt9U', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Dr. Mario', pic: '../images/mariospin3.png', desc: '', video: '', shop: 'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Super Mario Kart', pic: '../images/mariospin4.png', desc: '', video: 'https://www.youtube.com/embed/esNJUSMsDG4', shop: 'https://www.nintendo.com/store/products/super-nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Mario Kart 64', pic: '../images/mariospin5.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Tennis', pic: '../images/mariospin6.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Kart 8 Deluxe', pic: '../images/mariospin7.png', desc: '', video: '', shop: ''},
+    {name: 'Mario + Rabbids Kingdom Battle', pic: '../images/mariospin8.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Tennis Aces', pic: '../images/mariospin9.png', desc: '', video: '', shop: ''},
+    {name: 'Super Mario Party', pic: '../images/mariospin10.png', desc: '', video: '', shop: ''},
+    {name: 'Super Mario Maker 2', pic: '../images/mariospin11.png', desc: '', video: '', shop: ''},
+    {name: 'Paper Mario - The Origami King', pic: '../images/mariospin11.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Golf: Super Rush', pic: '../images/mariospin12.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Party Superstars', pic: '../images/mariospin13.png', desc: '', video: '', shop: ''},
+    {name: 'Mario Strikers: Battle League', pic: '../images/mariospin14.png', desc: '', video: '', shop: ''},
+    {name: 'Mario + Rabbids Sparks of Hope', pic: '../images/mariospin15.png', desc: '', video: '', shop: ''}
+]
+let zeldaJson = [
+    {name: 'The Legend of Zelda', pic: '../images/zelda1.png', desc: 'O primeiro jogo da série The Legend of Zelda , foi originalmente lançado no Japão como um jogo de lançamento para o Family Computer Disk System em fevereiro de 1986. O game incorpora elementos de jogos de RPG, ação e aventura. O jogador controla Link numa perspectiva aérea enquanto ele viaja num grande mapa ao ar livre com ambientes variados. O jogo está disponível no Nintendo Switch através do programa de benefícios para assinantes do Nintendo Switch Online, através do software Nintendo Entertainment System™ - Nintendo Switch Online', video: 'https://www.youtube.com/embed/1OpjeZMCsEU', shop:'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'Zelda 2: The Adventure of Link', pic: '../images/zelda2.png', desc:'O segundo jogo da serie de The Legend of Zelda, sendo uma continuação direta ao primeiro game. O jogo trás inovações no estilo de gameplay 2d, além da ja conhecida perspectiva aérea. O jogo está disponível no Nintendo Switch através do programa de benefícios para assinantes do Nintendo Switch Online, através do software Nintendo Entertainment System™ - Nintendo Switch Online', video:'https://www.youtube.com/embed/oCona7Kxqg8?start=28', shop:'https://www.nintendo.com/store/products/nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'The Legend of Zelda - A Link to the Past', pic: '../images/zelda8.png', desc: '', video: 'https://www.youtube.com/embed/esNJUSMsDG4', shop: 'https://www.nintendo.com/store/products/super-nintendo-entertainment-system-nintendo-switch-online-switch/'},
+    {name: 'The Legend of Zelda - Ocarina of Time', pic: '../images/zelda3.png'},
+    {name: "The Legend of Zelda - Majora's Mask", pic: '../images/zelda4.png'},
+    {name: 'The Legend of Zelda - Skyward Sword', pic: '../images/zelda5.png'},
+    {name: "The Legend of Zelda - Link's Awakening", pic: '../images/zelda6.png'},
+    {name: 'The Legend of Zelda - Breath of the Wild', pic: '../images/zelda7.png'}
+]
+let zeldaJsonSpinoff = [
+    {name: 'Hyrule Warriors - Definitive Edition', pic: '../images/zeldaspin1.png', desc:'', video:'https://www.youtube.com/embed/DUzdlDiFUyU', shop:'https://www.nintendo.com/store/products/hyrule-warriors-definitive-edition-switch/'},
+    {name: 'Hyrule Warriors - Age of Calamity', pic: '../images/zeldaspin2.png', desc:'', video:'https://www.youtube.com/embed/jmzvNn4682A', shop:'https://www.nintendo.com/store/products/hyrule-warriors-age-of-calamity-switch/'},
+    {name: 'Cadence of Hyrule', pic: '../images/zeldaspin3.png', desc:'', video:'https://www.youtube.com/embed/ATtLpjXTc7I', shop:'https://www.nintendo.com/store/products/cadence-of-hyrule-crypt-of-the-necrodancer-featuring-the-legend-of-zelda-switch/'}
+]
+
+
+
+
+let = bayonettaJson = [
+    {name: 'Bayonetta', pic: '../images/bayo1.png', desc:'', video:'https://www.youtube.com/embed/XN-lQc2slvM', shop:'https://www.nintendo.com/store/products/bayonetta-switch/'},
+    {name: 'Bayonetta 2', pic: '../images/bayo2.png', desc:'', video:'https://www.youtube.com/embed/3SxJGDofOfo', shop:'https://www.nintendo.com/store/products/bayonetta-2-switch/'},
+    {name: 'Bayonetta 3', pic: '../images/bayo3.png', desc:'', video:'https://www.youtube.com/embed/N6a5-8_kt3E', shop:'https://www.nintendo.com/store/products/bayonetta-3-switch/'}
+]
+
+// {name: '', pic: '../images/.png', desc: '', video: '', shop: ''}
---
+++ assets/javascript/script.js
@@ -0,0 +1,116 @@
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+function mario(e){
+    c('.main-series .games1').innerHTML = ''
+    c('.spin-offs .games1').innerHTML = ''
+    marioJson.map((item, index)=>{
+        let gameItem = c('.models .game').cloneNode(true)
+        gameItem.setAttribute('data-key', index);
+        gameItem.querySelector('.img-preview-area img').src = item.pic
+        gameItem.querySelector('.name-preview-area').innerHTML = item.name
+        gameItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault()
+            let key = e.target.closest('.game').getAttribute('data-key')
+            c('.game-title').innerHTML = marioJson[key].name
+            c('.desc-area').innerHTML = marioJson[key].desc
+            c('.button-div a').href = marioJson[key].shop
+            c('.trailer-area iframe').src = marioJson[key].video
+            c('.game-info-wait').classList.add('hide')
+            c('.game-info-area').classList.remove('hide')
+        })
+        c('.main-series .games1').append(gameItem)
+    })
+    marioJsonSpinoff.map((item, index)=>{
+        let gameItem = c('.models .game').cloneNode(true)
+        gameItem.setAttribute('data-key', index);
+        gameItem.querySelector('.img-preview-area img').src = item.pic
+        gameItem.querySelector('.name-preview-area').innerHTML = item.name
+        gameItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault()
+            let key = e.target.closest('.game').getAttribute('data-key')
+            c('.game-title').innerHTML = marioJsonSpinoff[key].name
+            c('.desc-area').innerHTML = marioJsonSpinoff[key].desc
+            c('.button-div a').href = marioJsonSpinoff[key].shop
+            c('.trailer-area iframe').src = marioJsonSpinoff[key].video
+            c('.game-info-wait').classList.add('hide')
+            c('.game-info-area').classList.remove('hide')
+        })
+        c('.spin-offs .games1').append(gameItem)
+    })
+    c('.game-info-area').classList.add('hide')
+    c('.game-info-wait').classList.remove('hide')
+    c('.spin-offs').classList.remove('hide')
+    //c('.game-selection-area .active').classList.remove('active')
+    //e.target.classList.add('active')
+}
+function zelda(e){
+    c('.main-series .games1').innerHTML = ''
+    c('.spin-offs .games1').innerHTML = ''
+    zeldaJson.map((item, index)=>{
+        let gameItem = c('.models .game').cloneNode(true)
+        gameItem.setAttribute('data-key', index);
+        gameItem.querySelector('.img-preview-area img').src = item.pic
+        gameItem.querySelector('.name-preview-area').innerHTML = item.name
+        gameItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault()
+            let key = e.target.closest('.game').getAttribute('data-key')
+            c('.game-title').innerHTML = zeldaJson[key].name
+            c('.desc-area').innerHTML = zeldaJson[key].desc
+            c('.button-div a').href = zeldaJson[key].shop
+            c('.trailer-area iframe').src = zeldaJson[key].video
+            c('.game-info-wait').classList.add('hide')
+            c('.game-info-area').classList.remove('hide')
+        })
+        c('.main-series .games1').append(gameItem)
+    })
+    zeldaJsonSpinoff.map((item, index)=>{
+        let gameItem = c('.models .game').cloneNode(true)
+        gameItem.querySelector('.img-preview-area img').src = item.pic
+        gameItem.querySelector('.name-preview-area').innerHTML = item.name
+        gameItem.setAttribute('data-key', index);
+        gameItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault()
+            let key = e.target.closest('.game').getAttribute('data-key')
+            c('.game-title').innerHTML = zeldaJsonSpinoff[key].name
+            c('.button-div a').href = zeldaJsonSpinoff[key].shop
+            c('.trailer-area iframe').src = zeldaJsonSpinoff[key].video
+            c('.game-info-wait').classList.add('hide')
+            c('.game-info-area').classList.remove('hide')
+        })
+        c('.spin-offs .games1').append(gameItem)
+    })
+    c('.game-info-area').classList.add('hide')
+    c('.game-info-wait').classList.remove('hide')
+    c('.spin-offs').classList.remove('hide')
+    c('.game-selection-area .active').classList.remove('active')
+    e.target.classList.add('active')
+}
+function bayonetta(e){
+    c('.main-series .games1').innerHTML = ''
+    c('.spin-offs .games1').innerHTML = ''
+    bayonettaJson.map((item, index)=>{
+        let gameItem = c('.models .game').cloneNode(true)
+        gameItem.setAttribute('data-key', index);
+        gameItem.querySelector('.img-preview-area img').src = item.pic
+        gameItem.querySelector('.name-preview-area').innerHTML = item.name
+        gameItem.querySelector('a').addEventListener('click', (e)=>{
+            e.preventDefault()
+            let key = e.target.closest('.game').getAttribute('data-key')
+            c('.game-title').innerHTML = bayonettaJson[key].name
+            c('.button-div a').href = bayonettaJson[key].shop
+            c('.trailer-area iframe').src = bayonettaJson[key].video
+            c('.game-info-wait').classList.add('hide')
+            c('.game-info-area').classList.remove('hide')
+        })
+        c('.main-series .games1').append(gameItem)
+    })
+    c('.game-info-area').classList.add('hide')
+    c('.game-info-wait').classList.remove('hide')
+    c('.spin-offs').classList.add('hide')
+    c('.game-selection-area .active').classList.remove('active')
+    e.target.classList.add('active')
+}
+
+
+mario()
